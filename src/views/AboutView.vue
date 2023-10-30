e31aaa988cd94f147473dee221ca3a456fc7a91d
JSNG11 <149286107+JSNG11@users.noreply.github.com>
2023-10-30 14:05:01 +0700

    Update AboutView.vue
    update struktur

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,7 +11,7 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold my-4">Struktur Desa</p>
-      <img src="@/assets/Struktur_desa.jpg" alt="" />
+      <img src="@/assets/Struktur_Desa.jpg" alt="" />
     </div>
   </main>
 </template>
